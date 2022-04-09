c9fe5055def446c830540baa2fb850d456377b61
austin-showen <austin.showen@gmail.com>
2022-04-09 16:15:39 -0700

    Add functionality to number buttons

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
+let currentValStr = "";
+let prevValStr = "";
 
 function operate(operand, a, b) {
     switch (operand) {
-        case "add":
-            return add(a, b);
-        case "subtract":
-            return subtract(a, b);
-        case "multiply":
-            return multiply(a, b);
-        case "divide":
-            return divide(a, b);
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return a / b;
     }
 }
 
-function onClick(e) {
-    console.log(this.textContent);
-    return this.textContent;
+function numberClick() {
+    currentValStr += this.textContent;
+    output.textContent = currentValStr;
+    return;
 }
 
-buttons = document.querySelectorAll("button");
-buttons.forEach(button => button.addEventListener("click", onClick));
+function operatorClick() {
+    console.log(this.textContent);
+    return;
+}
+
+function equalsClick() {
+    console.log(this.textContent);
+    return;
+}
+
+function clearClick() {
+    console.log(this.textContent);
+    return;
+}
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach(number => number.addEventListener("click", numberClick));
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => operator.addEventListener("click", operatorClick));
+
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", equalsClick);
+
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", clearClick);
+
+//todo: trim calculation if length > 13;
+//first round to 3? 4? 5? decimal places, then trim 
+
+output = document.querySelector(".output");
+output.textContent = 0; //todo: update output with every calculate operation
